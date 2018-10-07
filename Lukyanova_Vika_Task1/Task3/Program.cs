cbd40958fa2f6ed7bec5ffc4f4bcbbc04f965877
vika <vikusik_lukyanov@mail.ru>
2018-10-07 21:03:34 +0300

    Edit Task1

---
+++ Lukyanova_Vika_Task1/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int starLevel;
+            string number;
+            do
+            {
+                Console.Write("Input positive number ");
+                number = Console.ReadLine();
+            }
+            while (!(int.TryParse(number, out starLevel)) && starLevel < 0);
+            for (int i = 1; i < starLevel + 1; i++)
+            {
+                String sing = new string(' ', starLevel - i);
+                Console.Write(sing);
+                sing = new string('*', 1 + (i - 1) * 2);
+                Console.WriteLine(sing);
+
+            }
+        }
+    }
+}
