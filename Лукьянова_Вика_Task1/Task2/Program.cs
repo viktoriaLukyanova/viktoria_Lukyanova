3d604fefa8d71b1cde830665798feac622ee3b5b
vika <vikusik_lukyanov@mail.ru>
2018-10-07 19:34:39 +0300

    home work is completed

---
+++ Лукьянова_Вика_Task1/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int column;
+            string number;
+            do
+            {
+                Console.Write("Input number ");
+                number = Console.ReadLine();
+            }
+            while (!(int.TryParse(number, out column)) && column < 0);
+           
+             for (int i = 0; i < column; i++)
+             {
+                 String a = new string('*', i + 1);
+                 Console.WriteLine(a);
+             }
+        }
+    }
+}
