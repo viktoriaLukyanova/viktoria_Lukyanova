636ec3e0d8814ec82d50e84b7aea72b35d9db574
vika <vikusik_lukyanov@mail.ru>
2018-10-11 20:27:44 +0300

    On check. Task2

---
+++ Lukyanova_Vika_Task3/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2_1
+{
+    class Program
+    {
+        static void ArrayWriteLine(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.Write(element+"\0");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[15];
+            Random Rmd = new Random();            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Rmd.Next(100);
+            }
+            Array.Sort(arr);
+            Console.Write("Sorted massive: ");
+            ArrayWriteLine(arr);
+            Console.WriteLine($"\nMin:{arr[0]} and Max: {arr[14]}");
+            Console.ReadKey();
+        }
+    }
+}
