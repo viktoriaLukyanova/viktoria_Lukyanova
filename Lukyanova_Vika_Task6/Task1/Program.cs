a986051fe16df5945bdc9c8234cab64f28551a65
vika <vikusik_lukyanov@mail.ru>
2018-10-15 23:33:19 +0300

    Выполнено

---
+++ Lukyanova_Vika_Task6/Task1/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Program
+    {
+        class User
+        {
+            public string name;
+            public string lastName;
+            public string patronymical;
+            public int age;
+            DateTime birthday = new DateTime();
+            public User()
+            {
+                Name = name;
+                Birthday = birthday;
+              //  LastName = lastName;
+              //  Patronymical = patronymical;
+
+            }
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    if (string.IsNullOrEmpty(value) && int.TryParse(value, out int num))
+                    {
+                        Console.WriteLine("Неккоректный формат Name");
+                    }
+                    else
+                    {
+                        name = value;
+                    }
+                }
+
+            }
+            public DateTime Birthday
+            {
+                set
+                {
+                    if (value < DateTime.Now)
+                    {
+                        birthday = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Ошибка: Дата рождения указана некорректно");
+                    }
+                }
+            }
+            public int Age
+            {
+                get
+                {
+                    age = (DateTime.Today.AddYears(-birthday.Year)).Year;
+                    if (DateTime.Today.DayOfYear <= birthday.DayOfYear)
+                    {
+                        age++;
+                    }
+                    return age;
+                }
+            }
+            public string LastName
+            {
+                set
+                {
+                    if (string.IsNullOrEmpty(value) && int.TryParse(value, out int num))
+                    {
+                        throw new Exception("Ошибка: Неккоректно указана фамилия");
+                    }
+                    else
+                    {
+                        lastName = value;
+                    }
+
+                }
+            }
+            public string Patronymical
+            {
+                set
+                {
+                    if (string.IsNullOrEmpty(value) && int.TryParse(value, out int num))
+                    {
+                        throw new Exception("Ошибка: Неккоректно указано отчество");
+                    }
+                    else
+                    {
+                        Patronymical = value;
+                    }
+
+                }
+
+            }
+
+            class Employee : User
+            {
+                public string post;
+                public int experience;
+                public DateTime beganWork;
+                public Employee()
+                {
+                    Post = post;                    
+                    BeganWork = beganWork;
+                }
+                public string Post
+                {
+                    set
+                    {
+                        if (string.IsNullOrEmpty(value) && int.TryParse(value, out int num))
+                        {
+                            Console.WriteLine("Такой должности не существует");
+                        }
+                        else
+                        {
+                            post = value;
+                        }
+                    }
+                }
+                public int Experience
+                {
+                    get
+                    {
+                        experience = (DateTime.Today.AddYears(-beganWork.Year)).Year;
+                        if (DateTime.Today.DayOfYear <= beganWork.DayOfYear)
+                        {
+                            experience++;
+                        }
+                        return experience;
+                    }
+                }
+                public DateTime BeganWork
+                {
+                    get { return beganWork.Date; }
+                    set
+                    {
+                        if (value <= DateTime.Now.Date)
+                        {
+                            beganWork = value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: Неккоректная дата поступления");
+                        }
+                    }
+                }
+            }
+            static void Main(string[] args)
+            {
+                Employee Petr = new Employee();                
+                Petr.name = "Петр";
+                Petr.birthday = new DateTime(1997, 5, 10);
+                Petr.beganWork = new DateTime(2016, 10, 2);
+                Petr.post = "Программист";
+                Console.WriteLine(Petr.name + "\0 Возраст: " + Petr.Age + "\0 Начал работать: " + Petr.beganWork + "\0 Должность: " + Petr.post + "\0 Стаж: " + Petr.Experience);
+            }
+        }
+    }
+}
---
+++ Lukyanova_Vika_Task6/Task2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        public class Round
+        {
+            private double x, y, r;
+
+            public double Radius
+            {
+                get
+                {
+                    return r;
+                }
+
+                set
+                {
+                    x = y = 0;
+                    if (value <= 0)
+                    {
+                        throw new ArgumentException("Некорректное значение");
+                    }
+                    else
+                    {
+                        r = value;
+                    }
+
+                }
+            }
+            public double GetArea
+            {
+                get
+                {
+                    return Math.PI * r * r;
+                }
+            }
+
+            public double Length { get { return 2 * Math.PI * r; } }
+
+            class Ring : Round
+            {
+                public double rIn, xIn, yIn;                
+                public double R
+                {
+                    set
+                    {
+                        if (xIn == x && yIn == y)
+                            if (value >= 0)
+                            {
+                                if (value <= r)
+                                {
+                                    rIn = value;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ошибка: Внутренний радиус не может быть больше чем внешний");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка: Введен отрицательный радиус");
+                            }
+                        else
+                        {
+                            Console.WriteLine("Центры точек не совпадают");
+                        }
+                    }
+                    get { return rIn; }
+                }
+                public double AreaRing()
+                {
+                    return GetArea - Math.PI * rIn * rIn;
+                }
+                public double SumLength()
+                {
+                    return Length+ 2 * Math.PI * r;
+                }
+
+            }
+            static void Main(string[] args)
+            {
+                Ring rr = new Ring();
+                rr.Radius = 10;
+                rr.R = 0;
+                Console.WriteLine("площадь круга: " + rr.GetArea);
+                Console.WriteLine("длина круга: " + rr.Length);
+                Console.WriteLine("площадь кольца: "+rr.AreaRing());
+                Console.WriteLine("Сумма внешней и внутренней границ кольца: " + rr.SumLength());
+            }
+        }
+    }
+}
