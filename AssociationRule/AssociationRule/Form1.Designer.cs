facab12b71022ff0016acbe267e5c713f18b9731
viktoriaLukyanova <vikusik_lukyanov@mail.ru>
2019-03-17 14:12:19 +0300

    Ассоциативные правила

---
+++ AssociationRule/AssociationRule/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AssociationRule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataTrans = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Transact = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnGetAs = new System.Windows.Forms.Button();
+            this.openExcel = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTrans)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataTrans
+            // 
+            this.dataTrans.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTrans.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Transact});
+            this.dataTrans.Location = new System.Drawing.Point(12, 12);
+            this.dataTrans.Name = "dataTrans";
+            this.dataTrans.Size = new System.Drawing.Size(776, 286);
+            this.dataTrans.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID_Поля";
+            this.ID.Name = "ID";
+            // 
+            // Transact
+            // 
+            this.Transact.HeaderText = "Транзакции";
+            this.Transact.Name = "Transact";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 304);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(212, 28);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Загрузить файл с транзакциями";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(12, 338);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(212, 29);
+            this.btnClean.TabIndex = 2;
+            this.btnClean.Text = "Очистить таблицу транзакций";
+            this.btnClean.UseVisualStyleBackColor = true;
+            // 
+            // btnGetAs
+            // 
+            this.btnGetAs.Location = new System.Drawing.Point(672, 311);
+            this.btnGetAs.Name = "btnGetAs";
+            this.btnGetAs.Size = new System.Drawing.Size(116, 56);
+            this.btnGetAs.TabIndex = 3;
+            this.btnGetAs.Text = "Получить правила";
+            this.btnGetAs.UseVisualStyleBackColor = true;
+            // 
+            // openExcel
+            // 
+            this.openExcel.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetAs);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataTrans);
+            this.Name = "Form1";
+            this.Text = "Ассоциативные правила";
+            ((System.ComponentModel.ISupportInitialize)(this.dataTrans)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataTrans;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Transact;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnGetAs;
+        private System.Windows.Forms.OpenFileDialog openExcel;
+    }
+}
+
---
+++ AssociationRule/AssociationRule/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AssociationRule
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (openExcel.ShowDialog() == DialogResult.OK)
+            {
+                //Создаём приложение.
+                Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+                //Открываем книгу.                                                                                                                                                        
+                Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(openExcel.FileName, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+                //Выбираем таблицу(лист).
+                Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+                ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
+                //Очищаем от старого текста окно вывода.
+                dataTrans.Rows.Clear();
+                dataTrans.Columns.Clear();
+                for (int i = 1; i < 3; i++)
+                {
+                    var colum = new DataGridViewColumn();
+
+                    colum.CellTemplate = new DataGridViewTextBoxCell();
+                    if (i == 1)
+                    {
+                        colum.HeaderText = "ID_поля";
+                        colum.Width = 50;
+                        dataTrans.Columns.Add(colum);
+                    }
+                    else
+                    {
+                        colum.HeaderText = "Транзакты";
+                        colum.Width = 250;
+                        dataTrans.Columns.Add(colum);
+                    }
+                }
+                dataTrans.Rows.Add(7);
+                for (int i = 1; i < 6; i++)
+                {
+                    string a = "A";
+                    //Выбираем область таблицы. (в нашем случае просто ячейку)
+                    Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range(a + i.ToString(), a + i.ToString());
+                    // dataTrans.Rows[j].HeaderCell.Value = (i).ToString();
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if (j == 1)
+                        {
+                            string b = "B";
+                            Microsoft.Office.Interop.Excel.Range range1 = ObjWorkSheet.get_Range(b + i.ToString(), b + i.ToString());
+                            dataTrans[j, i - 1].Value = range1.Text;
+                        }
+                        else
+                        {
+                            dataTrans[j, i - 1].Value = range.Text;
+                        }
+                        Application.DoEvents();
+                    }
+                }
+                //Удаляем приложение (выходим из экселя) - ато будет висеть в процессах!
+                ObjExcel.Quit();
+            }
+
+        }
+        private void ReadDate()
+        {
+            List<string> Tran = new List<string>();
+            for (int i = 0; i < 7; i++)
+            {
+                Tran.Add(dataTrans[i, 1].ToString());
+            }
+
+        }
+    }
+}
